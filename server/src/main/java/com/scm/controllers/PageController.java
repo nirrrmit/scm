4a60639fa7058e27f0551b3365c180d4acccb7e3
Nirmit Singhal <nirmitcodes@gmail.com>
2024-09-29 04:04:37 +0530

    Setting up basic controller

---
+++ server/src/main/java/com/scm/controllers/PageController.java
@@ -0,0 +1,16 @@
+package com.scm.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+
+    @RequestMapping("/home")
+    public String home(Model model) {
+        model.addAttribute("firstName", "John");
+        return "home";
+    }
+
+}
