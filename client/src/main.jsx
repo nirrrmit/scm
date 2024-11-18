005ba0358e2518dd002c2b604da3b54ab4664597
Nirmit Singhal <nirmitcodes@gmail.com>
2024-11-18 18:19:03 +0530

    Use BrowserRouter instead of createBrowserRouter

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,23 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './styles/index.css'
 import Login from './pages/Login.jsx'
 import Test from './pages/Test';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Login />,
-  },
-  {
-    path: "/test",
-    element: <Test />,
-  },
-]);
-
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
+	<StrictMode>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Login />} />
+				<Route path="/test" element={<Test />} />
+			</Routes>
+		</BrowserRouter>
+	</StrictMode>
 )
